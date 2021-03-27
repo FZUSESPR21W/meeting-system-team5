44010c56c7d7788c13635434d13f5e1e46838923
Kosmos5 <1821748823@qq.com>
2021-03-27 20:43:18 +0800

    Revert "AddFooter"
    This reverts commit 3af190caa216be0497df96693b20a44507d4d88a.

--- client/src/views/secretary-user.vue
+++ client/src/views/secretary-user.vue
@@ -1,30 +1,24 @@
 <template>
   <div>
       <el-container style="height: 500px; border: 1px solid #eee">
-            <el-header style="text-align:right; font-size: 12px">
-                <span>用户名</span>
-            </el-header>
-            <el-container>
+          <el-header style="text-align:right; font-size: 12px">
+            <span>用户名</span>
+          </el-header>
+          <el-container>
               <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
 
-                <el-menu-item index="1"><i class="el-icon-user">用户信息</i></el-menu-item>
               </el-aside>
-                <el-container>
-                    <el-main>
-                        <el-table :data="tableDate">
-                        <el-table-column prop="username" label="用户ID" width="300">
-                        </el-table-column>
-                        <el-table-column prop="account" label="账号" width="300">
-                        </el-table-column>
-                        <el-table-column prop="register_date" label="注册时间">
-                        </el-table-column>
-                        </el-table>
-                    </el-main>
-                    <el-footer>
-                        footer
-                    </el-footer>
-                </el-container>
-            </el-container>
+              <el-main>
+                <el-table :data="tableDate">
+                  <el-table-column prop="username" label="用户ID" width="300">
+                  </el-table-column>
+                  <el-table-column prop="account" label="账号" width="300">
+                  </el-table-column>
+                  <el-table-column prop="register_date" label="注册时间">
+                  </el-table-column>
+                </el-table>
+              </el-main>
+          </el-container>
       </el-container>
   </div>
 </template>
