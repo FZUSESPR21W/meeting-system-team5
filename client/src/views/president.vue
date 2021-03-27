1bbbe0a8f733f45f597a43f46e309d8817e48469
weihaochen111 <786576329@qq.com>
2021-03-27 17:17:03 +0800

    建表

---
+++ client/src/views/president.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-table
+    :data="tableData"
+    height="600"
+    border
+    style="width: 100%">
+    <el-table-column
+      prop="president"
+      label="会议主席">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="会议议题">
+      
+    </el-table-column>
+    <el-table-column
+      prop="peopleCount"
+      label="会议人数">
+    </el-table-column>
+    <el-table-column
+      prop="time"
+      label="召开时间">
+    </el-table-column>
+  </el-table>
+</template>
