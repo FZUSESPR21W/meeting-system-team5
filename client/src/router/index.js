ea9ac9db0018830aec0481b168cc1d5cbf0d12e4
wammamms <49466377+wammamms@users.noreply.github.com>
2021-03-27 22:56:28 +0800

    Add files via upload

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,32 +4,51 @@ import usermeeting from '../views/usermeeting'
 import usermessage from '../views/usermessage'
 import secretaryPage2 from '../views/secretaryPage2'
 import secretaryUser from '../views/secretaryUser'
+import login from '../views/login'
+import register from '../views/register'
+import president from '../views/president'
+import submeeting from '../views/submeeting'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/usermeeting',
-    component: usermeeting
-  },
-  {
-    path: '/usermessage',
-    component: usermessage
-  },
-  {
-    path: '/secretaryPage2',
-    component: secretaryPage2
-  },
-  {
-    path: '/secretaryUser',
-    component: secretaryUser
-  }
+const routes = [{
+        path: '/usermeeting',
+        component: usermeeting
+    },
+    {
+        path: '/usermessage',
+        component: usermessage
+    },
+    {
+        path: '/secretaryPage2',
+        component: secretaryPage2
+    },
+    {
+        path: '/secretaryUser',
+        component: secretaryUser
+    },
+    {
+        path: '/login',
+        component: login
+    },
+    {
+        path: '/register',
+        component: register
+    },
+    {
+        path: '/president',
+        component: president
+    },
+    {
+        path: '/submeeting',
+        component: submeeting
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
