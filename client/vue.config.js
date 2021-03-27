96fb4d9378162fa49a8b10cc8dec93516fec73fb
seraph-1 <58468060+seraph-1@users.noreply.github.com>
2021-03-27 23:52:55 +0800

    更新config

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   lintOnSave: false,
+  devServer:{
+    host: "localhost",
+      port: 8080,
+      proxy: {
+          '/api': {
+              target: 'http://47.96.1.69',
+              changeOrigin: true,
+              pathRewrite: {
+                  '/api': ''
+              }
+          }
+      }
+}
 }
