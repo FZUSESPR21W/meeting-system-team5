b43934a50e9d2d61f8b6536a5153e49ef979cf4b
seraph-1 <58468060+seraph-1@users.noreply.github.com>
2021-03-27 16:02:08 +0800

    完善了布局和外观

--- client/src/views/usermeeting.vue
+++ client/src/views/usermeeting.vue
@@ -2,12 +2,16 @@
   <div id="body">
       <topline/>
       <div class="nav_left">
+          <img src="../assets/business.png" id="business">
           <a id="bbs">论坛列表</a>
-          <hr>
+          <div id="line1"></div>
+          <img src="../assets/message.png" id="message">
           <a id="msg">消息列表</a>
-          <hr>
+          <div id="line1"></div>
       </div>
       <div class="nav_right">
+          <a id="word">已经加入的论坛列表</a>
+          <hr>
         <el-table
             :data="tableData"
             stripe
@@ -66,18 +70,20 @@ export default {
 }
 .nav_left{
     margin-top: 20px;
-    border: rgb(102,102,102) 1px solid;
+    border: rgb(102,102,102) 2px solid;
     width: 15%;
     height: 100%;
     float: left;
+    border-radius: 10px;
 }
 .nav_right{
     margin-top: 20px;
-    border: rgb(102,102,102) 1px solid;
+    border: rgb(102,102,102) 2px solid;
     margin-left: 20px;
     width: 83%;
     height: 100%;
     float: left;
+    border-radius: 10px;
 }
 #bbs{
     width:100%;
@@ -88,5 +94,22 @@ export default {
     width:100%;
     height:50px;
     line-height: 50px;
+}
+#business{
+    display: flex;
+    position: relative;
+    left: 25%;
+    top: 5.5%;
+}
+#message{
+    display: flex;
+    position: relative;
+    left: 23%;
+    top: 6%;
+}
+#line1{
+    height: 2px;
+    width: 100%;
+    background: black;
 }
 </style>
